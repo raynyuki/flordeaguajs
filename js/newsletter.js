9e4072fe601572a4ab1c778cac74de3e3cb06a97
Agustina Firpo <86528571+raynyuki@users.noreply.github.com>
2021-09-23 15:27:38 -0300

    Add files via upload

---
+++ js/newsletter.js
@@ -0,0 +1,25 @@
+
+const URLJSON = 'https://jsonplaceholder.typicode.com/posts'
+
+ $('#btnSub').click(function () {
+
+    var emailData = $('input#email').val();
+    console.log(emailData);
+    if (emailData !== "") {
+        $.ajax({
+            url: URLJSON, 
+            method: 'POST',
+            data: {
+                email: emailData 
+            },
+            success: function (data) {
+                console.log(data);
+                $(`#email`).remove();
+                $(`#btnSub`).remove();
+                $(`#newsletter`).prepend(`<p>Gracias por subscribirte</p>`);
+            },
+        });
+    } else {
+        alert("Insert a email!");
+    }
+});
