a723eef9b6223b775dbe5a500d516054b6415c63
Agustina Firpo <86528571+raynyuki@users.noreply.github.com>
2021-09-27 11:59:12 -0300

    Add files via upload

---
+++ js/bienvenida.js
@@ -0,0 +1,28 @@
+
+
+let boton = document.getElementById("buttonLogin")
+boton.addEventListener ("click", respuestaClick)
+function respuestaClick (){
+    
+    let itemText = "Bienvenidx " + userName.value;
+    nombreBienvenida(itemText)
+    let borrarLi = document.getElementById("loginItem");
+    borrarLi.parentNode.removeChild(borrarLi);
+    
+    let listaUsers = userName.value;
+    localStorage.setItem("listaUsers", JSON.stringify(listaUsers))
+ 
+}
+
+let userName = document.getElementById("userName");
+let itemBienvenida = document.getElementById("user");
+
+function nombreBienvenida (itemText) {
+    let nombreUser = document.createElement("li");
+    let nombreUserText = document.createTextNode(itemText);
+    nombreUser.className = "nav-link bienvenidaUser"
+
+    nombreUser.appendChild(nombreUserText)
+    itemBienvenida.appendChild(nombreUser)
+}
+
