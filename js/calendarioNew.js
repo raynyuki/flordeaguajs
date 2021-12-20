e82373748ffeff2fea55451d9f0ea09cf83b6436
Agustina Firpo <86528571+raynyuki@users.noreply.github.com>
2021-12-20 17:27:45 -0300

    Add files via upload

--- js/calendarioNew.js
+++ js/calendarioNew.js
@@ -1,5 +1,5 @@
 
-//arrays de meses WIP
+//arrays de meses
 
 const año = [
     { id: 1, verduras: "Poroto Chaucha, Zapallito, Alcachofa", frutas: "Sandia"},
@@ -17,89 +17,120 @@ const año = [
 ]
 
 
+//jquery
 
- //WIP
 
  $(`.mes1`).on('click', function () {
           console.log()
           $(`.cardContainerMes`).empty();
-          $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[0].verduras}</li> Frutas: <li>${año[0].frutas}</li> </ul></div>`);
+          $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[0].verduras}</li> Frutas: <li>${año[0].frutas}</li> </ul></div> <div class="container-fluid d-flex justify-content-center align-items-center pt-5"> <button class="volverBtn"  onclick="location.reload()"/>Volver a calendario</button></div>`);
           }
 );
 
 $(`.mes2`).on('click', function () {
     console.log()
     $(`.cardContainerMes`).empty();
-    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[1].verduras}</li> Frutas: <li>${año[1].frutas}</li> </ul></div>`);
+    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[1].verduras}</li> Frutas: <li>${año[1].frutas}</li> </ul></div> <div class="container-fluid d-flex justify-content-center align-items-center pt-5"> <button class="volverBtn"  onclick="location.reload()"/>Volver a calendario</button></div>`);
     }
 );
 
 $(`.mes3`).on('click', function () {
     console.log()
     $(`.cardContainerMes`).empty();
-    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[2].verduras}</li> Frutas: <li>${año[2].frutas}</li> </ul></div>`);
+    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[2].verduras}</li> Frutas: <li>${año[2].frutas}</li> </ul></div> <div class="container-fluid d-flex justify-content-center align-items-center pt-5"> <button class="volverBtn"  onclick="location.reload()"/>Volver a calendario</button></div>`);
     }
 );
 
 $(`.mes4`).on('click', function () {
     console.log()
     $(`.cardContainerMes`).empty();
-    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[3].verduras}</li> Frutas: <li>${año[3].frutas}</li> </ul></div>`);
+    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[3].verduras}</li> Frutas: <li>${año[3].frutas}</li> </ul></div> <div class="container-fluid d-flex justify-content-center align-items-center pt-5"> <button class="volverBtn"  onclick="location.reload()"/>Volver a calendario</button></div>`);
     }
 );
 
 $(`.mes5`).on('click', function () {
     console.log()
     $(`.cardContainerMes`).empty();
-    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[4].verduras}</li> Frutas: <li>${año[4].frutas}</li> </ul></div>`);
+    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[4].verduras}</li> Frutas: <li>${año[4].frutas}</li> </ul></div>  <div class="container-fluid d-flex justify-content-center align-items-center pt-5"> <button class="volverBtn"  onclick="location.reload()"/>Volver a calendario</button></div>`);
     }
 );
 
 $(`.mes6`).on('click', function () {
     console.log()
     $(`.cardContainerMes`).empty();
-    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[5].verduras}</li> Frutas: <li>${año[5].frutas}</li> </ul></div>`);
+    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[5].verduras}</li> Frutas: <li>${año[5].frutas}</li> </ul></div><div class="container-fluid d-flex justify-content-center align-items-center pt-5"> <button class="volverBtn"  onclick="location.reload()"/>Volver a calendario</button></div>`);
     }
 );
 
 $(`.mes7`).on('click', function () {
     console.log()
     $(`.cardContainerMes`).empty();
-    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[6].verduras}</li> Frutas: <li>${año[6].frutas}</li> </ul></div>`);
+    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[6].verduras}</li> Frutas: <li>${año[6].frutas}</li> </ul></div> <div class="container-fluid d-flex justify-content-center align-items-center pt-5"> <button class="volverBtn"  onclick="location.reload()"/>Volver a calendario</button></div>`);
     }
 );
 
 $(`.mes8`).on('click', function () {
     console.log()
     $(`.cardContainerMes`).empty();
-    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[7].verduras}</li> Frutas: <li>${año[7].frutas}</li> </ul></div>`);
+    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[7].verduras}</li> Frutas: <li>${año[7].frutas}</li> </ul></div> <div class="container-fluid d-flex justify-content-center align-items-center pt-5"> <button class="volverBtn"  onclick="location.reload()"/>Volver a calendario</button></div>`);
     }
 );
 
 $(`.mes9`).on('click', function () {
     console.log()
     $(`.cardContainerMes`).empty();
-    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[8].verduras}</li> Frutas: <li>${año[8].frutas}</li> </ul></div>`);
+    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[8].verduras}</li> Frutas: <li>${año[8].frutas}</li> </ul></div><div class="container-fluid d-flex justify-content-center align-items-center pt-5"> <button class="volverBtn"  onclick="location.reload()"/>Volver a calendario</button></div>`);
     }
 );
 
 $(`.mes10`).on('click', function () {
     console.log()
     $(`.cardContainerMes`).empty();
-    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[9].verduras}</li> Frutas: <li>${año[9].frutas}</li> </ul></div>`);
+    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[9].verduras}</li> Frutas: <li>${año[9].frutas}</li> </ul></div> <div class="container-fluid d-flex justify-content-center align-items-center pt-5"> <button class="volverBtn"  onclick="location.reload()"/>Volver a calendario</button></div>`);
     }
 );
 
 $(`.mes11`).on('click', function () {
     console.log()
     $(`.cardContainerMes`).empty();
-    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[10].verduras}</li> Frutas: <li>${año[10].frutas}</li> </ul></div>`);
+    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[9].verduras}</li> Frutas: <li>${año[9].frutas}</li> </ul></div> <div class="container-fluid d-flex justify-content-center align-items-center pt-5"> <button class="volverBtn"  onclick="location.reload()"/>Volver a calendario</button></div>`);
     }
 );
 
 $(`.mes12`).on('click', function () {
     console.log()
     $(`.cardContainerMes`).empty();
-    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[11].verduras}</li> Frutas: <li>${año[11].frutas}</li> </ul></div>`);
+    $(`.cardContainerMes`).prepend(`<div class="calendario container-fluid d-flex justify-content-center align-items-center"> <ul> Verduras: <li>${año[11].verduras}</li> Frutas: <li>${año[11].frutas}</li> </ul></div> <div class="container-fluid d-flex justify-content-center align-items-center pt-5"> <button class="volverBtn"  onclick="location.reload()"/>Volver a calendario</button></div>`);
     }
 );
+
+
+
+//ajax
+
+const URLJSON = 'https://jsonplaceholder.typicode.com/posts'
+
+
+
+ $('#btnSub').click(function () {
+    
+    var emailData = $('input#email').val();
+    console.log(emailData);
+    if (emailData !== "") {
+        $.ajax({
+            url: URLJSON, 
+            method: 'POST',
+            data: {
+                email: emailData 
+            },
+            success: function (data) {
+                console.log(data);
+                $(`#email`).remove();
+                $(`#btnSub`).remove();
+                $(`#newsletter`).prepend(`<p>Gracias por subscribirte</p>`);
+            },
+        });
+    } else {
+        alert("Ingresa un email valido");
+    }
+});
--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 
 const URLJSON = 'https://jsonplaceholder.typicode.com/posts'
 
+
+
 $('#buttonLogin').click(function () {
 
-   var userData = $('input#userName').val();
-   console.log(userData);
+   let userData = $('input#userName').val();
    if (userData !== "") {
        $.ajax({
            url: URLJSON, 
@@ -13,41 +14,16 @@ $('#buttonLogin').click(function () {
                name: userData 
            },
            success: function (data) {
-               console.log(data);
+               
                $(`#userName`).remove();
                $(`#buttonLogin`).remove();
                $(`#loginItem`).prepend(`<p class="nav-link bienvenidaUser">Bienvenidx `+ userData + `</p>`);
            },
        });
-   } else {
+   } 
+   else {
        alert("Insert a name!");
    }
+
 });
 
-
-/*let boton = document.getElementById("buttonLogin")
-boton.addEventListener ("click", respuestaClick)
-function respuestaClick (){
-    
-    let itemText = "Bienvenidx " + userName.value;
-    nombreBienvenida(itemText)
-    let borrarLi = document.getElementById("loginItem");
-    borrarLi.parentNode.removeChild(borrarLi);
-    
-    let listaUsers = userName.value;
-    localStorage.setItem("listaUsers", JSON.stringify(listaUsers))
- 
-}
-
-let userName = document.getElementById("userName");
-let itemBienvenida = document.getElementById("user");
-
-function nombreBienvenida (itemText) {
-    let nombreUser = document.createElement("li");
-    let nombreUserText = document.createTextNode(itemText);
-    nombreUser.className = "nav-link bienvenidaUser"
-
-    nombreUser.appendChild(nombreUserText)
-    itemBienvenida.appendChild(nombreUser)
-}
-*/
